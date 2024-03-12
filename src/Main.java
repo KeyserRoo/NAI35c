7db12a7fe06b8ae868093f20e0632741f87129d5
s28970 <jgessek@gmail.com>
2024-03-12 16:49:35 +0100

    ver4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,14 +13,22 @@ public class Main {
 			k = 3;
 		String[][] test = getData("data\\iris_test.txt");
 		String[][] training = getData("data\\iris_training.txt");
-		// for (int i = 0; i < training.length; i++) {
-		// for (int j = 0; j < training[i].length; j++) {
-		// System.out.print(training[i][j]);
-		// }
-		// System.out.println();
-		// }
 		kNearestIndecies(test[0], training, k);
 	}
+
+	// public static String getType(String[] row){
+	// 	return row[row.length-1];
+	// }
+
+	// public static String getResult(int[] indecies, String[][] table){
+	// 	HashMap<String,Integer> map = new HashMap<>();
+	// 	map.put("Iris-setosa", 0);
+	// 	map.put("Iris-versicolor", 0);
+	// 	map.put("Iris-virginica", 0);
+
+	// 	Arrays.sort(indecies);
+
+	// }
 
 	public static int[] kNearestIndecies(String[] test, String[][] training, int k) {
 
@@ -29,7 +38,7 @@ public class Main {
 			distancesWithIndices[i][1] = i;
 		}
 
-		Arrays.sort(distancesWithIndices, (a, b) -> Double.compare(a[0], b[0]));
+		Arrays.sort(distancesWithIndices);
 
 		int[] closestIndecies = new int[k];
 		for (int i = 0; i < k; i++) {
@@ -45,20 +54,21 @@ public class Main {
 
 	public static double euclideanDistance(String[] test, String[] trainig) {
 		double[] temp = new double[test.length - 1];
-		for (int i = 0; i < test.length - 1; i++) {
+		for (int i = 0; i < test.length - 1; i++)
 			temp[i] = Math.pow(Double.parseDouble(test[i]) - Double.parseDouble(trainig[i]), 2);
-		}
+
 		double distance = 0;
-		for (int i = 0; i < temp.length; i++) {
+		for (int i = 0; i < temp.length; i++)
 			distance += temp[i];
-		}
+
 		return Math.sqrt(distance);
 	}
 
 	public static String[][] getData(String filePath) {
 		try {
 			List<String> lines = Files.readAllLines(Paths.get(filePath));
-			int noCols = lines.getFirst().split("\t").length;
+			// int noCols = lines.getFirst().split("\t").length;
+			int noCols = lines.get(0).split("\t").length;
 			int noRows = lines.size();
 
 			String[][] toReturn = new String[noRows][noCols];
@@ -72,5 +82,4 @@ public class Main {
 			return null;
 		}
 	}
-
 }
